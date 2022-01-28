d40ea29563380c48e692e6ed2b76b87f7b47124a
Michael Verschoof <m.verschoof@btcdirect.eu>
2022-01-28 16:23:47 +0100

    Added recursive component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,54 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script setup>
+import RecursiveComponent from './RecursiveComponent.vue';
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+const selected = ref([]);
 
-const count = ref(0)
+const select = (character, level) => {
+    selected.value[level] = character;
+    selected.value.splice(level + 1);
+};
+
+const characters = [
+    { name: 'Gandalf' },
+    {
+        name: 'Thranduil',
+        children: [
+            { name: 'Legolas' }
+        ]
+    },
+    {
+        name: 'Groin son of Farin',
+        children: [
+            {
+                name: 'Gloin son of Groin',
+                children: [
+                    { name: 'Gimli son of Gloin' }
+                ]
+            }
+        ]
+    }
+];
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>Hello recursive components</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <div id="container">
+        <recursive-component
+            :characters="characters"
+            :selected="selected"
+            :level="0"
+            @selected="select"
+        />
+    </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+#container {
+    border: 2px solid #41b883;
+    display: flex;
+    margin: 0 auto;
+    max-width: 800px;
 }
 </style>
